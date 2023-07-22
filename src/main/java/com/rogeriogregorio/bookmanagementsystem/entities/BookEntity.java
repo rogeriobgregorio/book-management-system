39f23176fe8b88ca2ab8873d57c9ae7d9353c63e
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2023-07-21 23:28:15 -0300

    properties, Book, ORM

---
+++ src/main/java/com/rogeriogregorio/bookmanagementsystem/entities/BookEntity.java
@@ -0,0 +1,91 @@
+package com.rogeriogregorio.bookmanagementsystem.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_books")
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description",columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "price")
+    private Double price;
+
+    public BookEntity() {
+    }
+
+    public BookEntity(String title, String author,
+                      String description, Double price) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookEntity that = (BookEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
