248d1ac4d2daec247f9832b2ccee3cf9ca243aa5
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2023-10-23 19:38:48 -0300

    CorsConfig, addCorsMappings

--- src/main/java/com/rogeriogregorio/bookmanagementsystem/config/CorsConfig.java
+++ src/main/java/com/rogeriogregorio/bookmanagementsystem/config/CorsConfig.java
@@ -1,25 +1,19 @@
-//package com.rogeriogregorio.bookmanagementsystem.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//@Configuration
-//public class CorsConfig {
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.addAllowedOrigin("*");
-//        config.addAllowedMethod(HttpMethod.GET);
-//        config.addAllowedMethod(HttpMethod.POST);
-//        config.addAllowedMethod(HttpMethod.PUT);
-//        config.addAllowedMethod(HttpMethod.DELETE);
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsFilter(source);
-//    }
-//}
+package com.rogeriogregorio.bookmanagementsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+    }
+}
--- src/main/java/com/rogeriogregorio/bookmanagementsystem/exceptions/BookAlreadyExistsException.java
+++ src/main/java/com/rogeriogregorio/bookmanagementsystem/exceptions/BookAlreadyExistsException.java
@@ -6,4 +6,3 @@ public class BookAlreadyExistsException extends RuntimeException {
         super(message);
     }
 }
-
